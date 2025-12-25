5d1922e5601a3c2a3d745d2062d3d2fe2fee733c
Karen Zavier <karensheronezavier@gmail.com>
2025-12-25 20:44:41 +0530

    Updated homepage with welcome message

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <div className="App">
+  <h1>Welcome to Green Closet</h1>
+  <p>Promoting sustainable consumption through second-hand clothing in Sri Lanka</p>
+</div>
+
+    
   );
 }
 
